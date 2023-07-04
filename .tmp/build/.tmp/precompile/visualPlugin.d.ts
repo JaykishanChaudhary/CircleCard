1808f9eede35bafa458fec9292b94abbb66cad40
Jaykishan <Jaykishanchaudhary678@gmail.com>
2023-07-04 14:40:35 +0530

    initial commit

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var circleCard042F8E96D33844C4BCC9BB8390AAC86D_DEBUG: IVisualPlugin;
+export default circleCard042F8E96D33844C4BCC9BB8390AAC86D_DEBUG;
